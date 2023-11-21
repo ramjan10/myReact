61a968927ca3ecec8a6a239ae5f03dd56f2d7287
ramjan10 <ramjan@efoli.com>
2023-11-21 10:38:35 +0600

    useReducer with useContext api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import CounterThree from "./components/UseReduce/CounterThree";
+import React, { useReducer } from 'react';
+import ComponentA from "./components/UseReduce/ComponentA";
 
+export  const counterContext = React.createContext();
+
+const initalState = 0;
+
+const reducer = (state, action) =>{
+   switch(action){
+     case 'increment':
+      return state + 1;
+     case 'decrement':
+      return state - 1;
+    default:
+      return state;
+   }
+}
 function App() {
+  const [count, dispatch] =  useReducer(reducer, initalState);
+
   return (
-    <div>
-      <CounterThree />
+    <div className="app">
+      <div> Count : {count}</div>
+
+      <counterContext.Provider value={{countDispatch: dispatch }}>
+            <ComponentA />
+      </counterContext.Provider>
     </div>
   );
 }
---
+++ src/components/UseReduce/ComponentB.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { counterContext } from '../../App';
+
+export default function ComponentB(){
+    const countContext = useContext(counterContext);
+
+    return(
+        <div>
+            <p>Component A</p>
+            <button type="button" onClick={()=> countContext.countDispatch('increment') }>Increment</button>
+            <button type="button" onClick={()=> countContext.countDispatch('decrement') }>Decrement</button>
+        </div>
+    );
+}
